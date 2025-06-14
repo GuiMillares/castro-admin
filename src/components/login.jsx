4f061c8a4cb4094768c3d0aa1f0a688385956000
Guilherme Millares <guilhermec.millares@gmail.com>
2025-06-14 17:22:02 -0300

    adjusting the name on the login screen

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, sendPasswordResetEmail } from "../firebase";
 import {
@@ -69,7 +69,7 @@ const Login = () => {
     <div className="main-content">
       <div className="left-panel">
         <div className="motivational-text">
-          <div className="brand">Syncro</div>
+          <div className="brand">Synchro</div>
           <h1>Agende com facilidade, gerencie com precisão.</h1>
           <p>
             Agendamentos automáticos, gestão de equipe e serviços tudo em um só
